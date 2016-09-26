ad5e1def9f913df977f8691da12b186099d3b976
bastei <bastei@nocontact>
2016-09-26 22:08:21 +0200

    Move sagaopt and add real saga device, update cherry picks

--- updates.sh
+++ updates.sh
@@ -8,16 +8,10 @@ if [ "${android}" = "" ]; then
 	android=~/android/system
 fi
 
-# Implement UICC TLV Data decoding.
-cherries+=(114381)
-
-# Skip unknown tags instead of aborting when parsing UICC TLV data
-cherries+=(114382)
-
 # UiccController: add back registerForOn
 cherries+=(114805)
 
-# UiccController: use registerForAvailable only when persist.radio.apm_sim_not_pwdn is enabled
-cherries+=(114806)
+# UiccController: add back registerForOn
+cherries+=(114805)
 
 ${android}/build/tools/repopick.py -b ${cherries[@]}
